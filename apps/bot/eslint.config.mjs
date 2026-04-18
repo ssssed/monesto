ae3e90d13f871c968051e6f2c71a0be577936553
Igor Koneshov <igorkoneshov@mail.ru>
2026-04-19 00:12:32 +0400

    fix(feature/server): правки после слияния

--- apps/bot/eslint.config.mjs
+++ apps/bot/eslint.config.mjs
@@ -1,3 +1,3 @@
-import { config } from '@repo/eslint-config/index.js';
+import { config } from '@monesto/eslint-config/index.js';
 
 export default [{ ignores: ['dist/**'] }, ...config];
--- apps/cli/eslint.config.js
+++ apps/cli/eslint.config.js
@@ -1,4 +1,4 @@
-import { config } from '@repo/eslint-config/index.js';
+import { config } from '@monesto/eslint-config/index.js';
 
 export default [...config];
 
